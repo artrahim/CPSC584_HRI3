d2e57b457298ccc5830b486342c0c713a5acb4a4
Artemiss Rahim <artemiss.rahim@ucalgary.ca>
2020-10-17 16:57:59 -0600

    progress on video streaming

--- src/index.js
+++ src/index.js
@@ -1,38 +1,76 @@
-// const bebop = require("node-bebop"),
+require('module-alias/register');
+
+var bebop = require("@lib/node-bebop"),
+    cv = require("opencv");
+
+var drone = bebop.createClient(),
+    mjpg = drone.getMjpegStream(),
+    buf = null,
+    w = new cv.NamedWindow("Video", 0);
+
+drone.connect(function() {
+    drone.MediaStreaming.videoEnable(1);
+});
+
+mjpg.on("data", function(data) {
+    buf = data;
+});
+
+setInterval(function() {
+    if (buf == null) {
+        return;
+    }
+
+    try {
+        cv.readImage(buf, function(err, im) {
+            if (err) {
+                console.log(err);
+            } else {
+                if (im.width() < 1 || im.height() < 1) {
+                    console.log("no width or height");
+                    return;
+                }
+                w.show(im);
+                w.blockingWaitKey(0, 50);
+            }
+        });
+    } catch(e) {
+        console.log(e);
+    }
+}, 100);
+
+
+// const bebop = require("@lib/node-bebop");
 //     fs = require("fs");
 //
-// const output = fs.createWriteStream("./video.h264"),
+// var output = fs.createWriteStream("./video.h264"),
 //     drone = bebop.createClient(),
 //     video = drone.getVideoStream();
-
+//
 // video.pipe(output);
-
-// start a live stream
-// drone.connect(() => {
+//
+// drone.connect(function() {
 //     drone.MediaStreaming.videoEnable(1);
 // });
 
-// take a 5 second recording with the drone
-// drone.connect(() => {
-//     drone.startRecording();
+// var bebop = require("@lib/node-bebop");
+//
+// var drone = bebop.createClient();
+//
+// drone.connect(function() {
+//     drone.takeOff();
 //
 //     setTimeout(function() {
-//         drone.stopRecording();
+//         drone.land();
+//
 //     }, 5000);
 // });
+// var bebop = require("@lib/node-bebop");
 //
-// drone.on('ready', () => {
-//     console.log('the drone has connected!');
+// var drone = bebop.createClient();
+//
+// drone.connect(function() {
+//     drone.MediaStreaming.videoStreamMode(0);
+//     drone.PictureSettings.videoStabilizationMode(3);
+//     drone.MediaStreaming.videoEnable(1);
 // });
-
-var bebop = require('../lib/node-bebop');
-
-var drone = bebop.createClient();
-
-drone.connect(function() {
-    drone.takeOff();
-
-    setTimeout(function() {
-        drone.land();
-    }, 5000);
-});
